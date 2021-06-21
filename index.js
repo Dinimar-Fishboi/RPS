4defa5ba61312aacab39463506fabcbf4c962ebe
Dinimar Fish <tayla@tallisman.com.au>
2021-06-21 22:29:53 +1000

    initial psuedocoding

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-var x = 2
-var y = 6
-console.log(x + y)
+//When user enters the screen, they should be asked 'R, P, or S'
+
+window.prompt("Hi! R, P or S?")
+
+//When the user enters 'R', 'P', or 'S' - that should be given a value
+
+//The computer should randomly choose either 'R', 'P', or 'S'
+
+// If both the User and the Computer choose the same value, it's a tie.
+
+// R > S, S > P, P > R, The computer should choose a variable and a specific
+// result should come up based on the user input and the computer input.
+
+// The amount of Losses, Wins and Ties should be displayed to the user
+// after the round. The stats will show up with an 'ok' button.
+
+// The user will be asked if they want to 'Play Again'
+
+// If they select 'OK' they return to the start. If they select 'Cancel'
+// The alert box will close.
