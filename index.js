973c71099d771078451cb6f1b21fca52f26c06a8
Dinimar Fish <tayla@tallisman.com.au>
2021-06-22 22:33:10 +1000

    var userAnswer and compAnswer

--- index.js
+++ index.js
@@ -1,80 +1,102 @@
 
+// Stores user response in variable
+var tagName = prompt("Please enter an HTML Tag (ex. h1, h2, p, div):", "enter tag");
 
 
 //When user enters the screen, they should be asked 'R, P, or S'
 
-window.prompt("Hi! Want to play?", "R, S or P?",)
+var userAnswer = window.prompt("Hi! Want to play?", "R, S or P?",)
 
-var userPrompt = {
-   userInput: ["R","P","S"],
-}
+console.log(userAnswer);
 
-function game () {
-    var ABC = 1;
-    var choice = window.prompt("Let's Play");
-        switch (choice) {
-            case "R":
-                console.log("You chose Rock");
-                var R = 1;
-                var P = 2;
-                var S = 0;
-                break;
-            case "P":
-                console.log("You chose Paper");
-                var R = 0;
-                var P = 1;
-                var S = 2;
-                break;
-            case "S":
-                console.log("You chose Scissors");
-                var R = 2;
-                var P = 0;
-                var S = 1;
-            default: 
-            console.log("Oh, so you don't want to play?")
-                break;
-        }
-}
+// var userPrompt = {
+//    userInput: ["R","P","S"],
+// }
 
-game ();
+// function game () {
+//     var ABC = 1;
+//     var choice = window.prompt("Let's Play");
+//         switch (choice) {
+//             case "R":
+//                 console.log("You chose Rock");
+//                 var R = 1;
+//                 var P = 2;
+//                 var S = 0;
+//                 var comp= [s, r, p]
+//                 break;
+//             case "P":
+//                 console.log("You chose Paper");
+//                 var R = 0;
+//                 var P = 1;
+//                 var S = 2;
+//                 break;
+//             case "S":
+//                 console.log("You chose Scissors");
+//                 var R = 2;
+//                 var P = 0;
+//                 var S = 1;
+//             default: 
+//             console.log("Oh, so you don't want to play?")
+//                 break;
+//         }
+// }
 
-//When the user enters 'R', 'P', or 'S' - that should be given a value
-// Whatever user enters, value is assigned value '1'
-// HOWEVER if user enters R, R=1, P=2, S=0.
-// HOWEVER if user enters S, R=2, P=0, S=1.
-// HOWEVER if user enters P, R=0, P=1, S=2.
+// game ();
 
-function compTurn() {
+// //When the user enters 'R', 'P', or 'S' - that should be given a value
+// // Whatever user enters, value is assigned value '1'
+// // HOWEVER if user enters R, R=1, P=2, S=0.
+// // HOWEVER if user enters S, R=2, P=0, S=1.
+// // HOWEVER if user enters P, R=0, P=1, S=2.
 
-}
+// function compTurn() {
 
-//The computer should randomly choose either 'R', 'P', or 'S'
+// }
+
+// //The computer should randomly choose either 'R', 'P', or 'S'
+
+// function getRandomInt(max) {
+//     return Math.floor(Math.random() * max);
+//   }
+
+//   // !!!!!!!!!! The above function randomly generates a number,
+//   // the below command prints that function.
+  
+//  var x = getRandomInt(5);
+//  console.log(x);
+
+//   console.log(getRandomInt(3));
+//  window.alert((getRandomInt(3)));
+
+
+
+// // If both the User and the Computer choose the same value, it's a tie.
+
+
+
+// // R > S, S > P, P > R, The computer should choose a variable and a specific
+// // result should come up based on the user input and the computer input.
+
+// // The amount of Losses, Wins and Ties should be displayed to the user
+// // after the round. The stats will show up with an 'ok' button.
+
+// // The user will be asked if they want to 'Play Again'
+
+// // If they select 'OK' they return to the start. If they select 'Cancel'
+// // The alert box will 
+
+// //What are the possible inputs from user?
+
+var possibleAnswers = ["R", "P", "S"]
+
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-  // !!!!!!!!!! The above function randomly generates a number,
-  // the below command prints that function.
-  
- var x = getRandomInt(5);
- console.log(x);
+  var randomIndex = getRandomInt(3);
 
-  console.log(getRandomInt(3));
- window.alert((getRandomInt(3)));
+ var compAnswer = possibleAnswers[randomIndex];
 
-
-// If both the User and the Computer choose the same value, it's a tie.
-
-
-
-// R > S, S > P, P > R, The computer should choose a variable and a specific
-// result should come up based on the user input and the computer input.
-
-// The amount of Losses, Wins and Ties should be displayed to the user
-// after the round. The stats will show up with an 'ok' button.
-
-// The user will be asked if they want to 'Play Again'
-
-// If they select 'OK' they return to the start. If they select 'Cancel'
-// The alert box will 
+ console.log(compAnswer);
